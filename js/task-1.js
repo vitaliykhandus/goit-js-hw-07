219584b533d66abb91d62063fa4befd444336c67
vitaliykhandus <97749247+vitaliykhandus@users.noreply.github.com>
2024-06-20 22:38:17 +0300

    first commit

---
+++ js/task-1.js
@@ -0,0 +1,10 @@
+const listItemAll = document.querySelectorAll(".item")
+
+
+const categories = `Number of categories: ${listItemAll.length}`;
+console.log(categories);
+
+
+const listText = document.querySelector(".item")
+const catetegory = `Category: ${listText.textContent}`;
+console.log(catetegory)
